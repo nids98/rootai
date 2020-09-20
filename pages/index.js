95748cb94c6243c9e8489fe2d4791d13d3348ab9
Nidhi Shetty <nidishetty98@gmail.com>
2020-09-20 19:20:53 +0530

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>rootai</title>
+      </Head>
+      <Layout>
+        <div>
+          Hello
+        </div>
+      </Layout>
+    </div>
+  )
+}
